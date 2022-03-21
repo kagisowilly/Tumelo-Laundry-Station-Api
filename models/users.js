498b4fd659f6a91cfd633c555f1be4fb350967ef
Kagiso Mphayi <91262652+Kagisomphayi@users.noreply.github.com>
2022-03-21 19:08:46 +0200

    Add files via upload

--- models/users.js
+++ models/users.js
@@ -17,18 +17,24 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
-  user_role: {
-    type: String,
-    required: true,
-  },
+  // user_role: {
+  //   type: String,
+  //   required: true,
+  // },
   join_date: {
     type: Date,
+    required: false,
+    default:Date.now,
   },
-//   booking: {
-//     type: Array,
-//     required: false,
-//     default: [],
-//   },
+  admin:{
+    type: Boolean,
+    default: false,
+  },
+  booking: {
+    type: Array,
+    required: false,
+    default: [],
+  },
 });
 
 module.exports = mongoose.model("Users", userSchema);
