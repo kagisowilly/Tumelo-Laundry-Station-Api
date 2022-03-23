e50dc355a1c33e2c3a9fe5577226b87d097bc6b6
Kagisomphayi <mphayiw@gmail.com>
2022-03-23 09:56:31 +0200

    changes

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -118,7 +118,7 @@ async function getUser(req, res, next) {
   try {
     user = await Users.findById(req.params.id);
     if (user == null) {
-      return res.status(404).json({ message: "Cannot Find user" });
+      return res.status(404).json({ message: "Cannot find user" });
     }
   } catch (err) {
     return res.status(500).json({ message: err.message });
